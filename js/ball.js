ce734df86c9cb6e728ce3f2ebd466f53d0e37d27
Harrison <hsfisher@email.neit.edu>
2026-05-01 19:22:41 -0400

    Adding Some "Flair"!
    Added ric flair as the ball

--- js/ball.js
+++ js/ball.js
@@ -1,3 +1,8 @@
+// Global Image Variable
+var img = new Image();
+img.src = "images/flair-ball.png";
+
+// Ball Class
 function Ball() {
     this.x = canvas.width / 2;
     this.y = canvas.height / 2;
@@ -6,20 +11,26 @@ function Ball() {
     this.vy = 0;
 }
 
+// Draw Ball
 Ball.prototype.draw = function () {
-    ctx.fillStyle = "blue";
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-    ctx.fill();
+    if (img) {
+        ctx.drawImage(
+            img,
+            this.x - this.size,
+            this.y - this.size,
+            this.size * 2,
+            this.size * 2
+        );
+    }
 };
 
-// 3-zone Bounce + Ricochet
+// Movement
 Ball.prototype.move = function () {
 
     this.x += this.vx;
     this.y += this.vy;
 
-    // Walls
+    // Top / bottom walls
     if (this.y - this.size < 0) {
         this.y = this.size;
         this.vy *= -1;
@@ -30,7 +41,7 @@ Ball.prototype.move = function () {
         this.vy *= -1;
     }
 
-    // Player 1
+    // Player 1 collision
     if (
         this.x - this.size < player1.x + player1.width &&
         this.x > player1.x &&
@@ -38,14 +49,13 @@ Ball.prototype.move = function () {
         this.y < player1.y + player1.height
     ) {
         this.x = player1.x + player1.width + this.size;
-
         this.vx = Math.abs(this.vx);
 
         var hitPos = (this.y - player1.y) / player1.height;
         this.vy = (hitPos - 0.5) * 10;
     }
 
-    // Player 2
+    // Player 2 collision
     if (
         this.x + this.size > player2.x &&
         this.x < player2.x + player2.width &&
@@ -53,14 +63,13 @@ Ball.prototype.move = function () {
         this.y < player2.y + player2.height
     ) {
         this.x = player2.x - this.size;
-
         this.vx = -Math.abs(this.vx);
 
         var hitPos = (this.y - player2.y) / player2.height;
         this.vy = (hitPos - 0.5) * 10;
     }
 
-    // Score
+    // Scoring
     if (this.x < 0) {
         p2Wins++;
         resetGame();
